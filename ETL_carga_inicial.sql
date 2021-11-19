9b2a57b28770b46ee104872300e2477b59460705
Gianlucca Devigili <gianluccadevigili@gmail.com>
2021-11-19 16:26:00 -0300

    Adiciona o ETL para Medicamento

--- ETL_carga_inicial.sql
+++ ETL_carga_inicial.sql
@@ -7,3 +7,14 @@ SELECT
     c.nome
 FROM DBCFB.dbo.Cliente c;
 
+-- Medicamento --
+
+-- INSERT INTO DwCFB.dbo.Medicamento
+SELECT 
+	NEWID(), 
+	m.ID_medicamento,
+	m.nome,
+	m.preco
+FROM DBCFB.dbo.Medicamento m
+
+-- Categoria
