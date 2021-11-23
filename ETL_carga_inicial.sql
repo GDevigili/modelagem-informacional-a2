cf760a57bd98ab9f9120cb8e4f6047a46f47a734
Gianlucca Devigili <gianluccadevigili@gmail.com>
2021-11-22 21:45:09 -0300

    Update ETL_carga_inicial.sql

--- ETL_carga_inicial.sql
+++ ETL_carga_inicial.sql
@@ -62,6 +62,7 @@ FROM DBCFB.dbo.Fornecedor f
 INSERT INTO DWCFB.dbo.Endereco
 SELECT 
 	NEWID(), 
+	e.ID_Endereco,
 	e.cep,
 	e.logradouro,
 	e.numero,
@@ -91,16 +92,70 @@ FROM DBCFB.dbo.Pedido p
 
 
 -------------
--- Receita_detail --
+-- Receita --
 -------------
 
+INSERT INTO DWCFB.dbo.Receita
 SELECT 
-	ped.ID_pedido,
+	NEWID(),
+	SUM(dw_m.preco * iem.quantidade) as 'Valor',
+	SUM(iem.quantidade) as 'UnidadesVendidas',
+	dw_f.ChaveFornecedor,
+	dw_e.ChaveEndereco,
+	dw_d.ChaveDia,
+	dw_cat.ChaveCategoria,
+	dw_m.ChaveMedicamento,
+	dw_c.ChaveCliente
+FROM DBCFB.dbo.Pedido ped
+	-- Incluído em
+	JOIN DBCFB.dbo.incluido_em iem
+		ON ped.ID_pedido = iem.ID_pedido
+	-- Medicamento
+	JOIN DBCFB.dbo.Medicamento db_m
+		ON db_m.ID_medicamento = iem.ID_medicamento
+	JOIN DWCFB.dbo.Medicamento dw_m
+		ON dw_m.ID_Medicamento = iem.ID_medicamento
+	-- Fornecedor
+	JOIN DBCFB.dbo.Fornecedor db_f
+		ON db_f.ID_fornecedor = db_m.ID_fornecedor
+	JOIN DWCFB.dbo.Fornecedor dw_f
+		ON dw_f.ID_Fornecedor = db_f.ID_fornecedor
+	-- Cliente
+	JOIN DWCFB.dbo.Cliente dw_c
+		ON dw_c.ID_Cliente = ped.ID_cliente
+	-- Endereco
+	JOIN DBCFB.dbo.Endereco db_e
+		ON dw_c.ID_Cliente = db_e.ID_cliente
+	JOIN DWCFB.dbo.Endereco dw_e
+		ON dw_e.CEP = db_e.cep
+	-- Dia
+	JOIN DWCFB.dbo.Dia dw_d
+		ON dw_d.DataCompleta = ped.data
+	-- Categoria
+	JOIN DWCFB.dbo.Categoria dw_cat
+		ON dw_cat.ID_Categoria = db_m.ID_categoria
+GROUP BY 
+	ped.ID_pedido, 
+	dw_f.ChaveFornecedor,
+	dw_e.ChaveEndereco,
+	dw_d.ChaveDia,
+	dw_cat.ChaveCategoria,
+	dw_m.ChaveMedicamento,
+	dw_c.ChaveCliente
+
+
+--------------------
+-- Receita_detail --
+--------------------
+
+INSERT INTO DWCFB.dbo.Receita_detail
+SELECT 
+	NEWID(),
 	SUM(dw_m.preco * iem.quantidade) as 'Valor',
 	SUM(iem.quantidade) as 'UnidadesVendidas',
 	DATEPART(hour, GETDATE()) as 'Hora',
-	dw_f.ChaveFornecedor,
 	dw_e.ChaveEndereco,
+	dw_f.ChaveFornecedor,
 	dw_d.ChaveDia,
 	dw_cat.ChaveCategoria,
 	dw_m.ChaveMedicamento,
@@ -142,4 +197,5 @@ GROUP BY
 	dw_cat.ChaveCategoria,
 	dw_m.ChaveMedicamento,
 	dw_c.ChaveCliente
+
 
